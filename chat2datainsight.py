463713f7228dea81522bde9e1fea113073b8b2a8
Daniel Konrad <mail@daniel-konrad.com>
2023-09-25 21:23:05 +0200

    more models and error handliing

--- chat2datainsight.py
+++ chat2datainsight.py
@@ -5,12 +5,14 @@ import warnings, os, json
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
 from langchain.callbacks import StreamlitCallbackHandler
-from langchain.agents import create_csv_agent, create_pandas_dataframe_agent
+from langchain.agents import create_pandas_dataframe_agent
 from langchain.agents.agent_types import AgentType
+from langchain.schema.output_parser import OutputParserException
+
 import matplotlib.pyplot as plt
 
 #Models
-available_models = {"ChatGPT-3.5": "gpt-3.5-turbo-0301", "ChatGPT-4": "gpt-4"}
+available_models = {"ChatGPT-3.5": "gpt-3.5-turbo-0301", "ChatGPT-4": "gpt-4","GPT-3": "text-davinci-003", "GPT-3.5 Instruct": "gpt-3.5-turbo-instruct"}
 
 #Chat Logic
 ##Load file
@@ -19,8 +21,12 @@ def load_file(filename):
     st.write(df.head())
     return df
 #Generate LLM Response
-def generate_openai_response(df, input_query, model_name='gpt-3.5-turbo-0301', temperature=0, callbacks=None):
-    llm = ChatOpenAI(model_name=model_name, temperature=temperature, openai_api_key=st.secrets.get("OPENAI_API_KEY"))
+def generate_openai_response(df, input_query, model_name='gpt-3.5-turbo-0301', temperature=0.1, callbacks=None):
+    response = None
+    if model_name in ("text-davinci-003", "gpt-3.5-turbo-instruct"):
+         llm = OpenAI(model_name=model_name, temperature=temperature, openai_api_key=st.secrets.get("OPENAI_API_KEY"))
+    elif model_name in ("gpt-3.5-turbo-0301", "gpt-3.5-turbo", "gpt-4"):
+         llm = ChatOpenAI(model_name=model_name, temperature=temperature, openai_api_key=st.secrets.get("OPENAI_API_KEY"))
     #df = load_file(file)
     #Pandas Dataframe Agent
     agent = create_pandas_dataframe_agent(
@@ -33,7 +39,27 @@ def generate_openai_response(df, input_query, model_name='gpt-3.5-turbo-0301', t
          )
 
     #Agent Query
-    response = agent(input_query, callbacks=callbacks)
+    try:
+        response = agent(input_query, callbacks=callbacks)
+    except Exception as e:
+        if type(e) == openai.error.APIError:
+            st.error("OpenAI API Error. Please try again a short time later. (" + str(e) + ")")
+        elif type(e) == openai.error.Timeout:
+            st.error("OpenAI API Error. Your request timed out. Please try again a short time later. (" + str(e) + ")")
+        elif type(e) == openai.error.RateLimitError:
+            st.error("OpenAI API Error. You have exceeded your assigned rate limit. (" + str(e) + ")")
+        elif type(e) == openai.error.APIConnectionError:
+            st.error("OpenAI API Error. Error connecting to services. Please check your network/proxy/firewall settings. (" + str(e) + ")")
+        elif type(e) == openai.error.InvalidRequestError:
+            st.error("OpenAI API Error. Your request was malformed or missing required parameters. (" + str(e) + ")")
+        elif type(e) == openai.error.AuthenticationError:
+            st.error("Please enter a valid OpenAI API Key. (" + str(e) + ")")
+        elif type(e) == openai.error.ServiceUnavailableError:
+            st.error("OpenAI Service is currently unavailable. Please try again a short time later. (" + str(e) + ")")
+        elif type(e) == OutputParserException:
+            st.error("Unfortunately the code generated from the model contained errors and was unable to execute or parsable.")
+        else:
+            st.error("Unfortunately the code generated from the model contained errors and was unable to execute.")
     return response
 
 #Frontend Logic
@@ -57,7 +83,7 @@ if st.session_state.get("dataset") is not None:
 prompt = st.text_area(":eyes: What would you like to analyze?",height=10)
 btn_go = st.button("Go...", use_container_width=True)
 if prompt and btn_go:
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=True)
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=True, collapse_completed_thoughts=False)
         df = st.session_state.get("dataset").copy()
         response = generate_openai_response(df, prompt, model_name=available_models.get(model_name), callbacks=[st_cb])
         st.write(response.get("output"))
